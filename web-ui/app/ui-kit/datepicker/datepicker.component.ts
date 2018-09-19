adf425866b2a2b51166dba76a8c13bbb212a501c
AlexanderLobov1995 <alex1995nazarovo@gmail.com>
2018-09-20 00:11:25 +0300

    add broken datepicker part 4

--- web-ui/app/ui-kit/datepicker/datepicker.component.ts
+++ web-ui/app/ui-kit/datepicker/datepicker.component.ts
@@ -1,9 +1,12 @@
 import {ChangeDetectionStrategy, Component, Input, OnInit} from "@angular/core";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 
-export type ViewTypes = 'month' | 'year' | 'multi-year';
-export type DayTypes = 'Mo'| 'Di'| 'Mi'| 'Do'| 'Fr'| 'Sa'| 'So';
-export type FirstDayOfWeekTypes = 'monday' | 'tuesday' |  'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday';
+export type ViewTypes = 'month' | 'year';
+export type DayTypes = 'Mo' | 'Di' | 'Mi' | 'Do' | 'Fr' | 'Sa' | 'So';
+
+export const LAST_MONTH = 11;
+export const FIRST_MONTH = 0;
+export const COUNT_DAY_OF_WEEK = 7;
 
 @Component({
   selector: 'app-datepicker',
@@ -11,13 +14,15 @@ export type FirstDayOfWeekTypes = 'monday' | 'tuesday' |  'wednesday' | 'thursda
   styleUrls: ['./datepicker.component.styl'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DatepickerComponent implements OnInit{
+export class DatepickerComponent implements OnInit {
 
-  @Input() startView: ViewTypes = 'month';
   @Input() firstDayOfWeek = 1;
+  @Input() startView: ViewTypes = 'month';
 
-  mask = [ /\d/, /\d/, '-',  /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
-  headerDays = ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'];
+  $currentView: ViewTypes;
+
+  mask = [/\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
+  defaultHeaderDays = ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'];
   months = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
 
   days: number[] = [];
@@ -26,8 +31,7 @@ export class DatepickerComponent implements OnInit{
   month = 0;
   year = 2018;
 
-
-  datepickerFormGroup: FormGroup
+  datepickerFormGroup: FormGroup;
 
   ngOnInit(): void {
 
@@ -41,97 +45,123 @@ export class DatepickerComponent implements OnInit{
     this.initCalendar();
   }
 
-  get inputValue(){
-    const input = this.datepickerFormGroup.get('input');
-    console.log(input && input.value);
-    return input && input.value || '';
+  initCalendar() {
+    this.$currentView = this.startView;
+    const currentDate = new Date();
+    this.month = currentDate.getMonth();
+    this.year = currentDate.getFullYear();
+    this.calculateDaysView(currentDate);
   }
 
   daysInMonth(date: Date) {
     const year = date.getFullYear();
     const month = date.getMonth();
-    return new Date(year, month+ 1, 0).getDate();
+    return new Date(year, month + 1, 0).getDate();
   }
 
-  firstDayInMonth(date: Date){
-    console.log(date)
+  firstDayInMonth(date: Date) {
     const year = date.getFullYear();
     const month = date.getMonth();
-    let day = new Date(year, month, 1).getDay();
-    // if (day === 0 && !this.isSundayFirstDay){
-    //   day = 7;
-    // }
-    return day;
+    return new Date(year, month, 1).getDay();
   }
 
-  initCalendar(){
-    // console.log(this.isSundayFirstDay)
-    const currentDate = new Date();
-    this.month = currentDate.getMonth();
-    this.year = currentDate.getFullYear();
-    this.calculateMonthView(currentDate);
-  }
-
-  get monthView(){
-    return this.months[this.month];
-  }
-
-  get yearView(){
-    return this.year;
-  }
-
-  get headers(){
-    const firstHeaders = this.headerDays.splice(0, this.firstDayOfWeek);
-    return this.headerDays.concat(firstHeaders);
-  }
-
-  calculateMonthView(date: Date){
+  calculateDaysView(date: Date) {
     const daysInMonth = this.daysInMonth(date);
-    console.log(daysInMonth)
     const firstDay = this.firstDayInMonth(date);
-    console.log(firstDay)
     const offsetDays = this.calculateOffsetDays(firstDay);
-    console.log(offsetDays)
-    for (let offsetDay = 0; offsetDay < offsetDays; offsetDay++){
+    for (let offsetDay = 0; offsetDay < offsetDays; offsetDay++) {
       this.offsets.push('');
     }
-    for (let day = 1; day <= daysInMonth; day++){
+    for (let day = 1; day <= daysInMonth; day++) {
       this.days.push(day);
     }
   }
 
-  calculateOffsetDays(firstDay: number){
-    if(this.firstDayOfWeek > firstDay){
-      return 7 - this.firstDayOfWeek + firstDay;
+  calculateOffsetDays(firstDay: number) {
+    if (this.firstDayOfWeek > firstDay) {
+      return COUNT_DAY_OF_WEEK - this.firstDayOfWeek + firstDay;
     }
     return firstDay - this.firstDayOfWeek;
   }
 
   goBack() {
-    if(this.month === 0){
-      this.month = 11;
-      this.year -=1;
-      return;
+    if (this.$currentView === 'month') {
+      this.goBackMonth();
+    } else {
+      this.goBackYear();
+    }
+    this.days = [];
+    this.offsets = [];
+    this.calculateDaysView(new Date(this.year, this.month, 1));
+  }
+
+  goNext() {
+    if (this.$currentView === 'month') {
+      this.goNextMonth();
+    } else {
+      this.goNextYear();
+    }
+    this.days = [];
+    this.offsets = [];
+    this.calculateDaysView(new Date(this.year, this.month, 1));
+  }
+
+  goBackMonth() {
+    if (this.month === FIRST_MONTH) {
+      this.month = LAST_MONTH;
+      this.year -= 1;
     } else {
       this.month -= 1;
     }
-    this.days = [];
-    this.offsets = [];
-    this.calculateMonthView(new Date(this.year, this.month, 1));
   }
 
-  goNext(){
-    if(this.month === 11){
-      this.month = 0;
-      this.year +=1;
-      return;
+  goNextMonth() {
+    if (this.month === LAST_MONTH) {
+      this.month = FIRST_MONTH;
+      this.year += 1;
     } else {
       this.month += 1;
     }
-    this.days = [];
-    this.offsets = [];
-    this.calculateMonthView(new Date(this.year, this.month, 1));
   }
 
+  goBackYear() {
+    this.year -= 1;
+  }
+
+  goNextYear() {
+    this.year += 1;
+  }
+
+  toggleView() {
+    if (this.$currentView === 'month') {
+      this.$currentView = 'year';
+    } else {
+      this.$currentView = 'month';
+    }
+  }
+
+  get monthWithYear() {
+    return this.$currentView === 'month' ? this.months[this.month] + ' ' + this.year : this.year;
+  }
+
+  get headerDays() {
+    const defaultHeader = [...this.defaultHeaderDays];
+    const firstHeaders = defaultHeader.splice(0, this.firstDayOfWeek);
+    return defaultHeader.concat(firstHeaders);
+  }
+
+  get isMonthCurrentView() {
+    return this.$currentView === 'month';
+  }
+
+  get currentView() {
+    return this.$currentView;
+  }
+
+  get inputValue() {
+    const input = this.datepickerFormGroup.get('input');
+    console.log(input && input.value);
+    return input && input.value || '';
+  }
 
 }
