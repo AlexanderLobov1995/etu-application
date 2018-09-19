44badae0efd6049af2241c7f8937b5d5917c5803
Alexander Lobov <aleksandr.lobov@t-systems.com>
2018-09-19 19:28:33 +0300

    add broken date picker part3

--- web-ui/app/ui-kit/datepicker/datepicker.component.ts
+++ web-ui/app/ui-kit/datepicker/datepicker.component.ts
@@ -1,25 +1,30 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {ChangeDetectionStrategy, Component, Input, OnInit} from "@angular/core";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 export type ViewTypes = 'month' | 'year' | 'multi-year';
 export type DayTypes = 'Mo'| 'Di'| 'Mi'| 'Do'| 'Fr'| 'Sa'| 'So';
+export type FirstDayOfWeekTypes = 'monday' | 'tuesday' |  'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday';
 
 @Component({
   selector: 'app-datepicker',
   templateUrl: './datepicker.component.html',
-  styleUrls: ['./datepicker.component.styl']
+  styleUrls: ['./datepicker.component.styl'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DatepickerComponent implements OnInit{
 
   @Input() startView: ViewTypes = 'month';
+  @Input() firstDayOfWeek = 1;
 
   mask = [ /\d/, /\d/, '-',  /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
-  headerDays: DayTypes[] = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'];
+  headerDays = ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'];
+  months = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
 
   days: number[] = [];
   offsets: string[] = [];
 
-  month: number;
+  month = 0;
+  year = 2018;
 
 
   datepickerFormGroup: FormGroup
@@ -33,7 +38,7 @@ export class DatepickerComponent implements OnInit{
 
 
     });
-    this.calculateMonthView();
+    this.initCalendar();
   }
 
   get inputValue(){
@@ -53,20 +58,39 @@ export class DatepickerComponent implements OnInit{
     const year = date.getFullYear();
     const month = date.getMonth();
     let day = new Date(year, month, 1).getDay();
-    if (day === 0){
-      day = 7;
-    }
+    // if (day === 0 && !this.isSundayFirstDay){
+    //   day = 7;
+    // }
     return day;
   }
 
-  calculateMonthView(){
+  initCalendar(){
+    // console.log(this.isSundayFirstDay)
     const currentDate = new Date();
-    console.log(currentDate.getDay());
-    const daysInMonth = this.daysInMonth(currentDate);
+    this.month = currentDate.getMonth();
+    this.year = currentDate.getFullYear();
+    this.calculateMonthView(currentDate);
+  }
+
+  get monthView(){
+    return this.months[this.month];
+  }
+
+  get yearView(){
+    return this.year;
+  }
+
+  get headers(){
+    const firstHeaders = this.headerDays.splice(0, this.firstDayOfWeek);
+    return this.headerDays.concat(firstHeaders);
+  }
+
+  calculateMonthView(date: Date){
+    const daysInMonth = this.daysInMonth(date);
     console.log(daysInMonth)
-    const firstDay = this.firstDayInMonth(currentDate);
+    const firstDay = this.firstDayInMonth(date);
     console.log(firstDay)
-    const offsetDays = firstDay === 0 ? firstDay: firstDay - 1;
+    const offsetDays = this.calculateOffsetDays(firstDay);
     console.log(offsetDays)
     for (let offsetDay = 0; offsetDay < offsetDays; offsetDay++){
       this.offsets.push('');
@@ -74,6 +98,39 @@ export class DatepickerComponent implements OnInit{
     for (let day = 1; day <= daysInMonth; day++){
       this.days.push(day);
     }
+  }
+
+  calculateOffsetDays(firstDay: number){
+    if(this.firstDayOfWeek > firstDay){
+      return 7 - this.firstDayOfWeek + firstDay;
+    }
+    return firstDay - this.firstDayOfWeek;
+  }
+
+  goBack() {
+    if(this.month === 0){
+      this.month = 11;
+      this.year -=1;
+      return;
+    } else {
+      this.month -= 1;
+    }
+    this.days = [];
+    this.offsets = [];
+    this.calculateMonthView(new Date(this.year, this.month, 1));
+  }
+
+  goNext(){
+    if(this.month === 11){
+      this.month = 0;
+      this.year +=1;
+      return;
+    } else {
+      this.month += 1;
+    }
+    this.days = [];
+    this.offsets = [];
+    this.calculateMonthView(new Date(this.year, this.month, 1));
   }
 
 
